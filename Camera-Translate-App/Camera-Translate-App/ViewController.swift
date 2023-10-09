4475ac74003d5a19daa092d7f71c5d90b5cc2a0c
Moon <1tjen@naver.com>
2023-10-09 22:25:50 +0900

    feat: 프로젝트 생성

---
+++ Camera-Translate-App/Camera-Translate-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera-Translate-App
+//
+//  Created by 조호준 on 2023/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
