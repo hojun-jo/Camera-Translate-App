8f67061567e112856c1608fa19fb0846065297bf
Moon <1tjen@naver.com>
2023-10-10 17:12:49 +0900

    chore: main 스토리보드 삭제 및 폴더 구조 정리

---
+++ Camera-Translate-App/Camera-Translate-App/Application/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  Camera-Translate-App
+//
+//  Created by 조호준 on 2023/10/09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
