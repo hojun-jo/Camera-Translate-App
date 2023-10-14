64e529ec885c77e0e21de9c43a29a67fdf82855c
Moon <1tjen@naver.com>
2023-10-15 00:24:21 +0900

    feat: UIColor extension에 색상 추가

--- Camera-Translate-App/Camera-Translate-App/Extension/UIColor+.swift
+++ Camera-Translate-App/Camera-Translate-App/Extension/UIColor+.swift
@@ -10,5 +10,7 @@ import UIKit
 extension UIColor {
     static let yellow1 = UIColor(red: 255/255, green: 213/255, blue: 94/255, alpha: 1)
     static let yellow2 = UIColor(red: 255/255, green: 232/255, blue: 94/255, alpha: 1)
-    static let yellow3 = UIColor(red: 255/255, green: 193/255, blue: 94/255, alpha: 1)
+    static let yellow3 = UIColor(red: 255/255, green: 251/255, blue: 94/255, alpha: 1)
+    static let orange1 = UIColor(red: 255/255, green: 193/255, blue: 94/255, alpha: 1)
+    static let orange2 = UIColor(red: 255/255, green: 167/255, blue: 94/255, alpha: 1)
 }
