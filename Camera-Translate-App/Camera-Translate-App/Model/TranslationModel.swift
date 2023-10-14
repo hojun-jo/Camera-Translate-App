cb2d33d2d0d3482d40441115803698c2a6a48d64
Moon <1tjen@naver.com>
2023-10-15 04:45:13 +0900

    feat: TranslationModel 생성

---
+++ Camera-Translate-App/Camera-Translate-App/Model/TranslationModel.swift
@@ -0,0 +1,12 @@
+//
+//  TranslationModel.swift
+//  Camera-Translate-App
+//
+//  Created by Moon on 2023/10/15.
+//
+
+struct TranslationModel {
+    var source: Language = .english
+    var target: Language = .korean
+    var translatedText: String = ""
+}
