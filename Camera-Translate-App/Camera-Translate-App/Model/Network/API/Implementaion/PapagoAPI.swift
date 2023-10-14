533b5435836500c302a98b00b29be096427f9103
Moon <1tjen@naver.com>
2023-10-14 16:21:23 +0900

    feat: PapagoAPI 타입 생성

---
+++ Camera-Translate-App/Camera-Translate-App/Model/Network/API/Implementaion/PapagoAPI.swift
@@ -0,0 +1,27 @@
+//
+//  PapagoAPI.swift
+//  Camera-Translate-App
+//
+//  Created by Moon on 2023/10/14.
+//
+
+import Foundation
+
+struct PapagoAPI: APIType {
+    var baseURL = "https://openapi.naver.com/v1/papago/n2mt"
+    var httpMethod = HTTPMethod.post
+    var headers: [String : String]? = [
+        "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Naver-Client-Id" : Bundle.papagoClientID,
+        "X-Naver-Client-Secret" : Bundle.papagoClientSecret
+    ]
+    var queryItems: [URLQueryItem]?
+    
+    init(source: Language, target: Language, text: String) {
+        queryItems = [
+            URLQueryItem(name: "source", value: source.identifier),
+            URLQueryItem(name: "target", value: target.identifier),
+            URLQueryItem(name: "text", value: text)
+        ]
+    }
+}
